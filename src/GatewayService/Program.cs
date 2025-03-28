72688bfc23e5b65a8b29ec2065ead5f87a87e2a8
Felipe Nunes Work <felipe.rudra@hotmail.com>
2025-03-28 15:44:24 -0300

    att

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,20 +1,35 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("customPolicy", b =>
+    {
+        b.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
+            .WithOrigins(builder.Configuration["ClientApp"]!);
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-
 app.UseCors();
+
+app.MapReverseProxy();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
-
 app.Run();
-
-
