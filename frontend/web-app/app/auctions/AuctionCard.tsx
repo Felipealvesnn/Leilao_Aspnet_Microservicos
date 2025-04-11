5896e25e4ec792fe1b581fe3dd2095c07ed4d759
Felipe Nunes Work <felipe.rudra@hotmail.com>
2025-04-11 16:24:04 -0300

    Atualiza componente AuctionCard para usar o componente Image do Next.js e adiciona configuração de padrões de imagem remota no Next.js

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import { Auction } from '../types/auction'
-
-
+import React from "react";
+import Image from "next/image";
+import { Auction } from "../types/auction";
 
 type Props = {
   auction: Auction;
@@ -9,11 +8,23 @@ type Props = {
 
 export default function AuctionCard({ auction }: Props) {
   return (
-    <div className="flex mb-2 flex-col gap-2 p-5 bg-gray-200 rounded-md">
-      <h1 className="text-2xl font-bold">{auction.make} {auction.model} ({auction.year})</h1>
-      <p>{auction.color} - {auction.mileage} km</p>
-      <img src={auction.imageUrl} alt={`${auction.make} ${auction.model}`} className="w-64 h-auto rounded-md" />
-      <p className="text-gray-500">Status: {auction.status}</p>
-    </div>
-  )
+    <a href="#">
+      <div className="relative w-full bg-gray-200 aspect-[16/10] rounded-lg overflow-hidden">
+        <h1 className="text-2xl font-bold">
+          {auction.make} {auction.model} ({auction.year})
+        </h1>
+        <p>
+          {auction.color} - {auction.mileage} km
+        </p>
+        <Image
+          fill
+          className="object-cover"
+          src={auction.imageUrl}
+          alt={`${auction.make} ${auction.model}`}
+        />
+
+        <p className="text-gray-500">Status: {auction.status}</p>
+      </div>
+    </a>
+  );
 }
--- frontend/web-app/next.config.ts
+++ frontend/web-app/next.config.ts
@@ -5,6 +5,23 @@ const nextConfig: NextConfig = {
     fetches:{
       fullUrl: true,
     }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "example.org",
+        port: "",
+        pathname: "/**",
+      },
+    ]
+
   }
   /* config options here */
 };
