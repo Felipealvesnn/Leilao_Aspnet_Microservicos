8685d0d122552fe050bb47e65c3f2a920f842c18
felipe alves nunes <felipe.alvesnn@gmail.com>
2025-04-22 21:46:51 -0300

    feat: adiciona componente Skeleton para carregamento e atualiza dependências

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,27 +1,33 @@
-'use client';
+"use client";
 import React from "react";
 import AuctionCard from "./AuctionCard";
 import AppPagination from "../components/AppPagination";
 import { getData } from "../actions/auctionsActions";
 import { Auction } from "../types/auction";
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 //debugger;
 
-
-
-export default  function Listings() {
+export default function Listings() {
   const [auction, setAuctions] = React.useState<Auction[]>([]);
   const [pageCount, setPageCount] = React.useState(0);
   const [PageNumber, setPageNumber] = React.useState(1);
 
   React.useEffect(() => {
-    getData(PageNumber).then(data => {
+    getData(PageNumber).then((data) => {
       setAuctions(data.results);
       setPageCount(data.pageCount);
     });
   }, [PageNumber]);
-  
- if (!auction || auction.length === 0) {
-    return <div>Loading...</div>;
+
+  if (!auction || auction.length === 0) {
+    return (
+      <div className="grid grid-cols-4 gap-6">
+        {Array.from({ length: 8 }).map((_, index) => (
+          <Skeleton key={index} height={200} borderRadius={10} />
+        ))}
+      </div>
+    )
   }
 
   return (
@@ -32,7 +38,11 @@ export default  function Listings() {
         ))}
       </div>
       <div className="flex justify-center mt-4">
-        <AppPagination currentPage={PageNumber} pageCount={pageCount}  pageChanged={setPageNumber}/>
+        <AppPagination
+          currentPage={PageNumber}
+          pageCount={pageCount}
+          pageChanged={setPageNumber}
+        />
       </div>
     </>
   );
