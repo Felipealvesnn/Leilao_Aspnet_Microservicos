28cfa8fa910becfb7de4744aff4b57f5a545fc58
Felipe Alves <felipe.alvesnn@gmail.com>
2025-05-09 16:27:31 -0300

    dfgdgd

--- frontend/web-app/app/actions/auctionsActions.ts
+++ frontend/web-app/app/actions/auctionsActions.ts
@@ -1,11 +1,12 @@
 "use server";
+import { env } from "process";
 import { PagedResult } from "../types";
 import { Auction } from "../types/auction";
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
   //const res = await fetch("http://localhost:7001/api/auctions?date=2024-12-18");
 
-  const res = await fetch(`http://localhost:7002/api/search${query}`);
+  const res = await fetch(`${env.SEARCH_URL_INTERNAL}/api/search${query}`);
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
