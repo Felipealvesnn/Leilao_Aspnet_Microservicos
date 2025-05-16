ee7eeb9db3af6b257afa2685ecf8ade767595bc2
Felipe Trabalho W2E <felipe.alvesnn@gmail.com>
2025-05-16 15:23:36 -0300

    feat: adiciona componentes de entrada e data ao formulário de leilão

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -1,20 +1,121 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Button } from "flowbite-react";
 import React from "react";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
+import DateInput from "../components/DateInput";
+import Input from "../components/Input";
+import { usePathname } from "next/navigation";
 
 export default function AuctionForm() {
+  const pathname = usePathname();
+  const {
+    control,
+    handleSubmit,
+    setFocus,
+    register,
+    reset,
+    formState: { isSubmitting, isValid, errors },
+  } = useForm({
+    mode: "onTouched",
+  });
 
-    const { control, handleSubmit, setFocus,register, reset, formState: { isSubmitting, isValid, errors } } = useForm({
-        mode: 'onTouched'
-    });
+  async function onSubmit(data: FieldValues) {
+    // try {
+    //   let id = "";
+    //   let res;
+    //   if (pathname === "/auctions/create") {
+    //     res = await createAuction(data);
+    //     id = res.id;
+    //   } else {
+    //     if (auction) {
+    //       res = await updateAuction(data, auction.id);
+    //       id = auction.id;
+    //     }
+    //   }
+    //   if (res.error) {
+    //     throw res.error;
+    //   }
+    //   router.push(`/auctions/details/${id}`);
+    // } catch (error: any) {
+    //   toast.error(error.status + " " + error.message);
+    // }
+  }
 
   return (
-    <form onSubmit={handleSubmit((data) => console.log(data))}>
-      <input {...register("firstName")} />
-      <input {...register("lastName", { required: true })} />
-      {errors.lastName && <p>Last name is required.</p>}
-      <input {...register("age", { pattern: /\d+/ })} />
-      {errors.age && <p>Please enter number for age.</p>}
-      <input type="submit" />
+    <form className="flex flex-col mt-3" onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        label="Make"
+        name="make"
+        control={control}
+        rules={{ required: "Make is required" }}
+      />
+      <Input
+        label="Model"
+        name="model"
+        control={control}
+        rules={{ required: "Model is required" }}
+      />
+      <Input
+        label="Color"
+        name="color"
+        control={control}
+        rules={{ required: "Color is required" }}
+      />
+      <div className="grid grid-cols-2 gap-3">
+        <Input
+          label="Year"
+          name="year"
+          type="number"
+          control={control}
+          rules={{ required: "Year is required" }}
+        />
+        <Input
+          label="Mileage"
+          name="mileage"
+          control={control}
+          rules={{ required: "Mileage is required" }}
+        />
+      </div>
+      {pathname === "/auctions/create" && (
+        <>
+          <Input
+            label="Image URL"
+            name="imageUrl"
+            control={control}
+            rules={{ required: "Image url is required" }}
+          />
+          <div className="grid grid-cols-2 gap-3">
+            <Input
+              label="Reserve price (leave blank for no reserve)"
+              name="reservePrice"
+              type="number"
+              control={control}
+            />
+            <DateInput
+              label="Auction end date/time"
+              name="auctionEnd"
+              dateFormat="dd MMMM yyyy h:mm a"
+              showTimeSelect
+              control={control}
+              rules={{ required: "Auction end date is required" }}
+            />
+          </div>
+        </>
+      )}
+
+      <div className="flex justify-between">
+        <Button outline color="gray">
+          Cancel
+        </Button>
+        <Button
+          disabled={!isValid || isSubmitting}
+          outline
+          color="success"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </div>
     </form>
   );
 }
