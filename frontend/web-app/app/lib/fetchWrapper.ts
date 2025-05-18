daa72bb65d54c66993d255b82fc9d0e96756ef90
felipe alves nunes <felipe.alvesnn@gmail.com>
2025-05-18 19:52:32 -0300

    feat: implementa função para realizar requisições GET com autenticação

---
+++ frontend/web-app/app/lib/fetchWrapper.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+ import { auth } from '@/auth';
+
+const baseUrl = process.env.SEARCH_URL_INTERNAL;
+
+export async function get(url: string) {
+    const requestOptions = {
+        method: 'GET',
+        headers: await getHeaders()
+    }
+
+    const response = await fetch(baseUrl + url, requestOptions);
+
+    return handleResponse(response);
+}
+
+async function getHeaders() {
+    const session = await auth();
+    const headers = {
+        'Content-type': 'application/json'
+    } as any;
+    if (session?.accessToken) {
+        headers.Authorization = 'Bearer ' + session.accessToken
+    }
+    return headers;
+}
+
+
+async function handleResponse(response: Response) {
+    const text = await response.text();
+    let data;
+    try {
+        data = JSON.parse(text);
+    } catch (error) {
+        data = text;
+    }
+
+    if (response.ok) {
+        return data || response.statusText;
+    } else {
+        const error = {
+            status: response.status,
+            message: typeof(data === 'string') ? data : response.statusText
+        }
+        return {error}
+    }
+}
