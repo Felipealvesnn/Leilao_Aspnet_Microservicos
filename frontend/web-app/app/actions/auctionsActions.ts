46b5ee7316d6c790ef491ad06ecec91de9cb05a3
Felipe Trabalho W2E <felipe.alvesnn@gmail.com>
2025-05-19 09:22:07 -0300

    feat: adiciona funções para criar, atualizar, deletar e obter detalhes de leilões

--- frontend/web-app/app/actions/auctionsActions.ts
+++ frontend/web-app/app/actions/auctionsActions.ts
@@ -1,8 +1,10 @@
 "use server";
 // import { env } from "process";
-import { PagedResult } from "../types";
+import { Bid, PagedResult } from "../types";
 import { Auction } from "../types/auction";
 import { fetchWrapper } from "../lib/fetchWrapper";
+import { FieldValues } from "react-hook-form";
+import { revalidatePath } from "next/cache";
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
   //const res = await fetch("http://localhost:7001/api/auctions?date=2024-12-18");
@@ -17,3 +19,28 @@ export async function updateAuctionTest() {
     return await fetchWrapper.put('/api/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c', data);
 }
 
+export async function createAuction(data: FieldValues) {
+    return await fetchWrapper.post('auctions', data);
+}
+
+export async function getDetailedViewData(id: string): Promise<Auction> {
+    return await fetchWrapper.get(`auctions/${id}`)
+}
+
+export async function updateAuction(data: FieldValues, id: string) {
+    const res = await fetchWrapper.put(`auctions/${id}`, data);
+    revalidatePath(`/auctions/${id}`)
+    return res;
+}
+
+export async function deleteAuction(id: string) {
+    return await fetchWrapper.del(`/api/auctions/${id}`);
+}
+
+export async function getBidsForAuction(id: string): Promise<Bid[]> {
+    return await fetchWrapper.get(`/api/bids/${id}`);
+}
+
+export async function placeBidForAuction(auctionId: string, amount: number) {
+    return await fetchWrapper.post(`/api/bids?auctionId=${auctionId}&amount=${amount}`, {})
+}
